0975339c5a7d9fe452bf245a4094e8f77ce23638
Aniruddhsinh Vala <155964024+aniruddhsinh-911@users.noreply.github.com>
2025-02-13 12:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,27 +21,3 @@ console.log(firstNonRepeatedChar('aabbcdd')); // 'c'
 console.log(firstNonRepeatedChar('aabbcc'));  // null
 console.log(firstNonRepeatedChar('abcde'));   // 'a'
 console.log(firstNonRepeatedChar(''));        // null
-
-    const charCount = {};
-    
-    // Count occurrences of each character
-    for (let char of str) {
-        charCount[char] = (charCount[char] || 0) + 1;
-    }
-    
-    // Find the first non-repeating character
-    for (let char of str) {
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-    
-    return null;
-}
-
-// Test cases
-console.log(firstNonRepeatedChar('aabbcdd')); // 'c'
-console.log(firstNonRepeatedChar('aabbcc'));  // null
-console.log(firstNonRepeatedChar('abcde'));   // 'a'
-console.log(firstNonRepeatedChar(''));        // null
-
